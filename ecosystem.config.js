5068591271feb4be097e1ffdf1a74ba8c3756d01
azadov-azamat <azamatazadov112@gmail.com>
2024-07-13 15:57:56 +0500

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,29 +2,41 @@ module.exports = {
     apps: [
         {
             name: "admin-bot",
-            script: "./bot/admin.js", // admin bot fayl yo'li
+            script: "./bot/admin.js",
             watch: true,
-            exec_mode: "fork",
+            exec_mode: "cluster",
+            instances: "max",
             interpreter: "node",
             env: {
                 NODE_ENV: "development"
             },
             env_production: {
                 NODE_ENV: "production"
-            }
+            },
+            max_memory_restart: "1G",
+            out_file: "/dev/null", // stdout log yozilmasin
+            error_file: "/dev/null", // stderr log yozilmasin
+            log_file: "/dev/null", // umumiy log yozilmasin
+            time: true // jarayonning vaqtini ko'rsatish
         },
         {
             name: "client-bot",
-            script: "./bot/client.js", // client bot fayl yo'li
+            script: "./bot/client.js",
             watch: true,
-            exec_mode: "fork",
+            exec_mode: "cluster",
+            instances: "max",
             interpreter: "node",
             env: {
                 NODE_ENV: "development"
             },
             env_production: {
                 NODE_ENV: "production"
-            }
+            },
+            max_memory_restart: "1G",
+            out_file: "/dev/null",
+            error_file: "/dev/null",
+            log_file: "/dev/null",
+            time: true
         }
     ]
 };
