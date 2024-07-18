8973106f93d0a916c557ec8c70624d1774a40825
azadov-azamat <azamatazadov112@gmail.com>
2024-07-18 14:08:18 +0500

    icons

--- keyboards/index.js
+++ keyboards/index.js
@@ -1,17 +1,17 @@
 const {Markup} = require("telegraf");
 
 const commandGroupButtons = Markup.keyboard([
-    ['Guruh qo\'shish', 'Guruh o\'zgartirish'],
-    ['Guruh o\'chirish', 'Guruhlar ro\'yhati']
+    ['Guruh qo\'shish ➕', 'Guruh o\'zgartirish 🔄'],
+    ['Guruh o\'chirish 🗑', 'Guruhlar ro\'yhati 🤔']
 ]).resize();
 
 const commandChannelButtons = Markup.keyboard([
-    ['Kanal qo\'shish', 'Kanal o\'zgartirish'],
-    ['Kanal o\'chirish', 'Kanallar ro\'yhati']
+    ['Kanal qo\'shish ➕', 'Kanal o\'zgartirish 🔄'],
+    ['Kanal o\'chirish 🗑', 'Kanallar ro\'yhati 🤔']
 ]).resize();
 
 const commandClearAds = Markup.inlineKeyboard([
-    Markup.button.callback('Tasdiqlash', 'confirm_clear_all'),
+    Markup.button.callback('Tasdiqlash ✅', 'confirm_clear_all'),
     Markup.button.callback('Bekor qilish', 'cancel_clear_all')
 ])
 
